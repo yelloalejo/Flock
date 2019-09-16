db8acd4cb8f70838479d122206f73d5dea55a955
yelloalejo <quindialejo@gmail.com>
2019-09-16 14:24:21 -0500

    Creating MainActivity, with searchbar on toolbar

--- app/src/main/java/com/flock/views/HomeActivity.kt
+++ app/src/main/java/com/flock/views/HomeActivity.kt
@@ -3,14 +3,14 @@ package com.flock.views
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.flock.R
+import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
-
+        setSupportActionBar(toolbar)
 
     }
-
 }
